6abf36ffb130edca59a19ddc2e1eeaf76041638d
Milos Vasiljkovic <milosvasiljkovic@m4systems.com>
2014-02-06 16:12:44 +0100

    Time Row added

--- index.php
+++ index.php
@@ -14,6 +14,8 @@ and open the template in the editor.
         <?php
         // put your code here
         echo date('d-m-Y');
+        echo '<br />';
+        echo time();
         ?>
     </body>
 </html>
